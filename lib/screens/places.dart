64a3ca27646c61c1124951d74a6f5a24bdba42bd
Chris Hsu <chris515@gmail.com>
2024-12-27 18:13:11 +0800

    first commit

---
+++ lib/screens/places.dart
@@ -0,0 +1,63 @@
+import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/screens/add_place.dart';
+import 'package:favorite_places/widgets/places_list.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PlacesScreen extends ConsumerStatefulWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  ConsumerState<PlacesScreen> createState() {
+    return _PlacesScreenState();
+  }
+}
+
+class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('我的地點'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (ctx) => AddPlaceScreen(),
+                ),
+              );
+            },
+            icon: Icon(Icons.add),
+          ),
+        ],
+        backgroundColor: Theme.of(context).navigationBarTheme.backgroundColor,
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FutureBuilder(
+            future: _placesFuture,
+            builder: (context, snapshot) =>
+                snapshot.connectionState == ConnectionState.waiting
+                    ? Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : PlacesList(
+                        places: userPlaces,
+                      ),
+          )),
+    );
+  }
+}
